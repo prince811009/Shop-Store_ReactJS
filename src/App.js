e04f3d67d04c02569eba764a3f72ede43bd5e727
prince811009 <prince811009fy@gmail.com>
2020-04-26 13:34:00 +0800

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -32,7 +32,6 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-
           console.log(this.state);
         });
       }
@@ -52,7 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignOutPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 import FormInput from '../form-input/form-input.conponent';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.style.scss';
+import './sign-in.style.scss'
+
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -14,49 +15,55 @@ class SignIn extends React.Component {
     this.state = {
       email: '',
       password: ''
+    };
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
-  }
-
-  handleSubmit = event => {
-    event.prevenDefault();
-
-    this.setState({ email: '', password: ''})
-  }
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
-  }
+  };
 
   render() {
-    return(
-      <div className="sign-in">
+    return (
+      <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <FormInput 
-            name="email" 
-            type="email"
+          <FormInput
+            name='email'
+            type='email'
             handleChange={this.handleChange}
             value={this.state.email}
-            label="email"
-            required 
+            label='email'
+            required
           />
-          <FormInput 
-            name="password" 
-            type="password"
-            handleChange={this.handleChange}
+          <FormInput
+            name='password'
+            type='password'
             value={this.state.password}
-            label="password"
-            required 
+            handleChange={this.handleChange}
+            label='password'
+            required
           />
-          <div className="buttons">
-            <CustomButton type="submit">Sign in</CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                {' '}
-                Sign in with Google{' '}
+              Sign in with Google
             </CustomButton>
           </div>
         </form>
