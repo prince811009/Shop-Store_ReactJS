571e0acf8ed403706b2282a6bc837612e1b9ee93
prince811009 <prince811009fy@gmail.com>
2020-05-10 16:37:24 +0800

    added new component named footer

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
+import Footer from './components/footer/footer.component';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -63,6 +65,7 @@ class App extends React.Component {
             }
           />
         </Switch>
+        <Footer />
       </div>
     );
   }
