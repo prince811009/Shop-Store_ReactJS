9eb7bd8d783ae0ea42b88540ad8168f7f58660d2
prince811009 <prince811009fy@gmail.com>
2020-05-10 12:35:04 +0800

    added new images to shop.data.js

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
   sections: [
     {
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/NYfvY4s/hat-australian-aussie-3326728.jpg',
       id: 1,
       linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/g3nhRB4/jackets.jpg',
       id: 2,
       linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/ZVm23xC/sneakers.jpg',
       id: 3,
       linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/Y3m9VT7/womens.jpg',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/kB6D3XB/men.jpg',
       size: 'large',
       id: 5,
       linkUrl: 'shop/mens'
